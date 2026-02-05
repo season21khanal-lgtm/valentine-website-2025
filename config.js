23f29a37550464cbc0e92e9e43421258efd1e3af
season21khanal-lgtm <season21khanal@gmail.com>
2026-02-05 07:06:51 +0545

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/didjkdf2l/video/upload/v1770254168/Sajjan_20Raj_20Vaidya_20-_20Dhairya_20_5BOfficial_20Release_5D_20_5B6FEsFvZ-hqY_5D_poknwu.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
